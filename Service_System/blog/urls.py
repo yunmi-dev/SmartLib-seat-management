61679e21789e5c8a9b59f3e4b76960dda82b2f6b
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-12-19 11:15:40 +0900

    feat: 서버, 웹캠 정상 동작 확인

--- Service_System/blog/urls.py
+++ Service_System/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.shortcuts import redirect
 from . import views
 from rest_framework import routers
 
@@ -6,8 +7,11 @@ router = routers.DefaultRouter()
 router.register('Post', views.BlogImages)
 router.register('Seat', views.SeatViewSet)
 
+def redirect_to_api(request):
+    """루트 URL을 API로 리다이렉트"""
+    return redirect('/api_root/')
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', redirect_to_api, name='home'),  # 이 줄 추가!
     path('api_root/', include(router.urls)),
 ]
